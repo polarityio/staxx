04f9863c5adeeffe90157b19ffb1a6d13e1921ed
Ed <ed@polarity.io>
2017-08-02 10:55:22 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Polarity STAXX Integration
+
+Polarity's STAXX integration gives users access to automated MD5, SHA1, SHA256, IPv4, IPv6 and Domain lookups within Anomali's STAXX platform..
+
+Anomali STAXX gives you a free, easy way to subscribe to any STIX / TAXII feed. Simply download the STAXX client, configure your data sources, and STAXX will handle the rest.
+
+To learn more about Anomali STAXX please see their official website at [https://www.anomali.com/platform/staxx](https://www.anomali.com/platform/staxx)
+
+
+| ![image](https://user-images.githubusercontent.com/306319/28879739-eda4e1ba-7770-11e7-9891-c520231a8a24.png)  |
+|---|
+|*Anomali STAXX Examples* |
+
+## STAXX Integration Options
+
+### Anomali STAXX Server URL
+
+The URL for your STAXX server which should include the schema (i.e., http, https) and port if required.  For example `https://192.168.1.29:8080`
+
+### Username
+
+Your Anomali STAXX username
+
+### Password
+
+The password for the provided STAXX user
+
+### Minimum Severity Level
+
+A string value which specifies the minimum severity level required for an indicator to be displayed.   For example, if you set the value to high then only indicators with a severity level of "high" or "very-high" will be displayed in the notification overlay.
+
+Allowed values are "low", "medium", "high", "very-high"
+
+## Minimum Confidence Level
+
+An integer value between 0 and 100 which specifies the minimum confidence level required for an indicator to be displayed.   For example, if you set the value to 55 then only indicators with a confidence of 55 or above will be displayed in the notification overlay.
+
+## Ignore Private IPs
+
+If set to true, private IPs (RFC 1918 addresses) will not be looked up (includes 127.0.0.1, 0.0.0.0, and 255.255.255.255)
+
+## Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see: 
+
+https://polarity.io/
---
+++ config/config.js
@@ -0,0 +1,150 @@
+module.exports = {
+    /**
+     * Name of the integration which is displayed in the Polarity integrations user interface
+     *
+     * @type String
+     * @required
+     */
+    name: "Anomali STAXX",
+    /**
+     * The acronym that appears in the notification window when information from this integration
+     * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+     * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+     * here will be carried forward into the notification window.
+     *
+     * @type String
+     * @required
+     */
+    acronym: "STX",
+    /**
+     * Description for this integration which is displayed in the Polarity integrations user interface
+     *
+     * @type String
+     * @optional
+     */
+    description: "Anomali STAXX integration for IP addresses and hashes",
+    entityTypes: ['IPv4', 'IPv6', 'hash', 'domain'],
+    /**
+     * An array of style files (css or less) that will be included for your integration. Any styles specified in
+     * the below files can be used in your custom template.
+     *
+     * @type Array
+     * @optional
+     */
+    "styles": [
+        "./styles/staxx.less"
+    ],
+    /**
+     * Provide custom component logic and template for rendering the integration details block.  If you do not
+     * provide a custom template and/or component then the integration will display data as a table of key value
+     * pairs.
+     *
+     * @type Object
+     * @optional
+     */
+    block: {
+        component: {
+            file: "./components/staxx.js"
+        },
+        template: {
+            file: "./templates/staxx.hbs"
+        }
+    },
+    request: {
+        // Provide the path to your certFile. Leave an empty string to ignore this option.
+        // Relative paths are relative to the STAXX integration's root directory
+        cert: '',
+        // Provide the path to your private key. Leave an empty string to ignore this option.
+        // Relative paths are relative to the STAXX integration's root directory
+        key: '',
+        // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+        // Relative paths are relative to the STAXX integration's root directory
+        passphrase: '',
+        // Provide the Certificate Authority. Leave an empty string to ignore this option.
+        // Relative paths are relative to the STAXX integration's root directory
+        ca: '',
+        // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+        // the url parameter (by embedding the auth info in the uri)
+        proxy: '',
+        /**
+         * If set to false, the integeration will ignore SSL errors.  This will allow the integration to connect
+         * to STAXX servers without valid SSL certificates.  Please note that we do NOT recommending setting this
+         * to false in a production environment.
+         */
+        rejectUnauthorized: false
+    },
+    logging: {
+        // directory is relative to the this integrations directory
+        // e.g., if the integration is in /app/polarity-server/integrations/virustotal
+        // and you set directoryPath to be `integration-logs` then your logs will go to
+        // `/app/polarity-server/integrations/integration-logs`
+        // You can also set an absolute path.  If you set an absolute path you must ensure that
+        // the directory you specify is writable by the `polarityd:polarityd` user and group.
+
+        //directoryPath: '/var/log/polarity-integrations',
+        level: 'trace',  //trace, debug, info, warn, error, fatal
+    },
+    /**
+     * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+     * as an array of option objects.
+     *
+     * @type Array
+     * @optional
+     */
+    options: [
+        {
+            key: "url",
+            name: "Anomali STAXX Server URL",
+            description: "The URL for your STAXX server which should include the schema (i.e., http, https) and port if required",
+            default: "",
+            type: "text",
+            userCanEdit: false,
+            adminOnly: true
+        },
+        {
+            key: "username",
+            name: "Username",
+            description: "The username of the STAXX user you want the integration to authenticate as",
+            default: "",
+            type: "text",
+            userCanEdit: true,
+            adminOnly: false
+        },
+        {
+            key: "password",
+            name: "Password",
+            description: "The password for the provided username you want the integration to authenticate as",
+            default: '',
+            type: "password",
+            userCanEdit: true,
+            adminOnly: false
+        },
+        {
+            key: "minimumSeverity",
+            name: "Minimum Severity Level",
+            description: "The minimum severity level required for indicators to be displayed [low, medium, high, very-high]",
+            default: 'low',
+            type: "text",
+            userCanEdit: true,
+            adminOnly: false
+        },
+        {
+            key: "minimumConfidence",
+            name: "Minimum Confidence Level",
+            description: "The minimum confidence level required for indicators to be displayed",
+            default: 0,
+            type: "number",
+            userCanEdit: true,
+            adminOnly: false
+        },
+        {
+            key: "ignorePrivateIps",
+            name: "Ignore Private IPs",
+            description: "If set to true, private IPs (RFC 1918 addresses) will not be looked up (includes 127.0.0.1, 0.0.0.0, and 255.255.255.255)",
+            default: true,
+            type: "boolean",
+            userCanEdit: false,
+            adminOnly: true
+        }
+    ]
+};
