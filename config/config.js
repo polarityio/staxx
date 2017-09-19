6c5b09326567e93cbce846d63d781b433d7d3836
Ed <ed@polarity.io>
2017-09-19 13:21:28 -0400

    Set default logging level info

--- config/config.js
+++ config/config.js
@@ -82,7 +82,7 @@ module.exports = {
         // the directory you specify is writable by the `polarityd:polarityd` user and group.
 
         //directoryPath: '/var/log/polarity-integrations',
-        level: 'trace',  //trace, debug, info, warn, error, fatal
+        level: 'info',  //trace, debug, info, warn, error, fatal
     },
     /**
      * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
